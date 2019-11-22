e2c157ab2b3a558b414c30655721c44e3bed03b1
Jorge Lopez <JorgeLopez5252@gmail.com>
2019-11-21 17:09:38 -0700

    created new layout

---
+++ db/db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Breakfast Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cowboy Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Bacon Western Burger", 0);
